ac1fb427b03ef934482d8e753c939c72acfa1a9a
khalilurrahmanmahdi <khalilurrahmanmahdi24@gmail.com>
2026-03-15 08:13:33 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,49 +7,34 @@ import Nav from "@/components/navbar/Nav"
 import Preload from "@/components/preload/Preload"
 import Projects from "@/components/projects/Projects"
 import { AnimatePresence } from "framer-motion"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function Home() {
   const [loadingPreloader, setLoadingPreloader] = useState<boolean>(true)
-  const [endedLoading, setEndedLoading] = useState<boolean>(false)
 
-  useEffect(() => {
-    const body = document.querySelector("body")
-
-    if (loadingPreloader) {
-      body?.classList.add("overflow-hidden")
-      setTimeout(() => {
-        setLoadingPreloader(false)
-      }, 4000)
-      setTimeout(() => {
-        setEndedLoading(true)
-      }, 3000)
-    } else {
-      body?.classList.remove("overflow-hidden")
-    }
-  }, [loadingPreloader])
-
-  if (loadingPreloader) {
-    return (
-      <>
-        <AnimatePresence mode="wait" initial={true}>
-          {loadingPreloader && <Preload endedLoading={endedLoading} />}
-        </AnimatePresence>
-      </>
-    )
+  const handleComplete = () => {
+    setLoadingPreloader(false)
   }
 
-  if (!loadingPreloader) {
-    return (
-      <>
-        <Nav />
-        <main data-scroll-container className="flex flex-col items-center">
-          <Hero />
-          <About />
-          <Projects />
-          <Contact />
-        </main>
-      </>
-    )
-  }
+  return (
+    <>
+      <AnimatePresence mode="wait" initial={true}>
+        {loadingPreloader && (
+          <Preload onComplete={handleComplete} />
+        )}
+      </AnimatePresence>
+
+      {!loadingPreloader && (
+        <>
+          <Nav />
+          <main data-scroll-container className="flex flex-col items-center">
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+          </main>
+        </>
+      )}
+    </>
+  )
 }
